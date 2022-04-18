d6252ea693bd67079344b730089522735c79836d
Rohan Singh <73023108+BlackHattedMiner@users.noreply.github.com>
2022-04-18 16:38:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,12 +20,11 @@ answer = c/o2
 if st.button('Get Answer'):
 	if answer <= 100:
      		st.write("You need", answer, "%", "in term 2 reach your goal.")
+			if t1 = 69:
+				st.warning("I see you are a man of culture ಠ‿↼")
 	else:
 		st.error('Required Percentage is above 100. Matlab itna lana impossible hai :(')
 		
-if st.button('Get Answer'):
-	if t1 = 69:
-		st.warning("I see you are a man of culture ಠ‿↼")
 
 with st.sidebar:
 #	if st.button('Study Resources'):
